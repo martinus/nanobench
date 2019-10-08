9af04839d52b55a411487d3bec2c760ac579f0b7
Martin Ankerl <martin.ankerl@gmail.com>
2019-10-08 19:01:43 +0200

    mark unreliable

--- src/include/nanobench.h
+++ src/include/nanobench.h
@@ -467,11 +467,8 @@ private:
         os << detail::fmt::Number(7, 1, mdapsSecPerIter * 100) << "% |";
 
         // 5th column: possible symbols, possibly errormessage, benchmark name
-        if (mdapsSecPerIter >= 0.1) {
-            os << " :bangbang:";
-        } else if (mdapsSecPerIter >= 0.05) {
-            os << " :question:";
-        } else if (mdapsSecPerIter >= 0.02) {
+        if (mdapsSecPerIter >= 0.05) {
+            // >=5%
             os << " :wavy_dash:";
         }
         os << ' ' << detail::fmt::MarkDownCode(name) << std::endl;
