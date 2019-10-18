da03e886f5e1138bcc461b433d74f7228ffc43f0
Martin Ankerl <martin.ankerl@gmail.com>
2019-10-18 14:48:06 +0200

    moar builds

--- src/scripts/all.sh
+++ src/scripts/all.sh
@@ -28,8 +28,6 @@ function build() {
     cd ${ORIGINDIR}
 }
 
-build "clang++" "11" "OFF" "-m32"
-
 #build "g++-4.9" "11" "OFF" "-m32"
 #build "g++-4.9" "14" "OFF" "-m32"
 
@@ -45,22 +43,44 @@ build "clang++" "11" "OFF" "-m32"
 #build "g++-7" "14" "OFF" "-m32"
 #build "g++-7" "17" "OFF" "-m32"
 
+#build "clang++" "11" "ON" "-m32" # linker error in chrono
+#build "clang++" "14" "ON" "-m32" # linker error in chrono
+#build "clang++" "17" "ON" "-m32" # linker error in chrono
+#build "clang++" "20" "ON" "-m32" # linker error in chrono
+build "clang++" "11" "ON"
+build "clang++" "14" "ON"
+build "clang++" "17" "ON"
+build "clang++" "20" "ON"
+build "clang++" "11" "OFF" "-m32"
+build "clang++" "14" "OFF" "-m32"
+build "clang++" "17" "OFF" "-m32"
+build "clang++" "20" "OFF" "-m32"
+build "clang++" "11" "OFF"
+build "clang++" "14" "OFF"
+build "clang++" "17" "OFF"
+build "clang++" "20" "OFF"
 build "g++" "11" "ON" "-m32"
 build "g++" "14" "ON" "-m32"
 build "g++" "17" "ON" "-m32"
 build "g++" "20" "ON" "-m32"
+build "g++" "11" "ON"
+build "g++" "14" "ON"
+build "g++" "17" "ON"
+build "g++" "20" "ON"
+build "g++" "11" "OFF" "-m32"
+build "g++" "14" "OFF" "-m32"
+build "g++" "17" "OFF" "-m32"
+build "g++" "20" "OFF" "-m32"
+build "g++" "11" "OFF"
+build "g++" "14" "OFF"
+build "g++" "17" "OFF"
+build "g++" "20" "OFF"
+
 
 build "clang++-6" "11" "OFF" "-m32"
 build "clang++-6" "14" "OFF" "-m32"
 build "clang++-6" "17" "OFF" "-m32"
 build "clang++-6" "20" "OFF" "-m32"
-
-build "clang++" "11" "OFF" "-m32"
-#build "clang++" "11" "ON" "-m32"
-#build "clang++" "17" "ON" "-m32"
-build "clang++" "17" "OFF" "-m32"
-#build "clang++" "20" "ON" "-m32"
-build "clang++" "20" "OFF" "-m32"
 
 
 # all the rest
