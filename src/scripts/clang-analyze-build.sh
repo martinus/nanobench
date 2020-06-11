1ccb45b3e6656d5f7538e00fea111f153ec45d10
Martin Ankerl <martin.ankerl@gmail.com>
2020-06-11 12:39:44 +0200

    better script

--- src/scripts/clang-analyze-build.sh
+++ src/scripts/clang-analyze-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-rm -f CMakeCache.txt
-rm -Rvf CMakeFiles
+rm -Rvf CMakeCache.txt session.data CMakeFiles
 ccache -C
 
-CXX=clang++ cmake -G Ninja -DCMAKE_CXX_FLAGS="-O2 -ftime-trace" -DCMAKE_BUILD_TYPE=Release ../..
+CXX=clang++ cmake -G Ninja -DNB_cxx_standard=17 -DCMAKE_CXX_FLAGS="-O2 -ftime-trace" -DCMAKE_BUILD_TYPE=Release ../..
+# CXX=clang++ cmake -G Ninja -DCMAKE_CXX_FLAGS="-ftime-trace" -DCMAKE_BUILD_TYPE=Debug ../..
 
 ~/git/ClangBuildAnalyzer/bin/ClangBuildAnalyzer --start .
 time ninja
