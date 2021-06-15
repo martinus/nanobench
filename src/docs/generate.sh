e0a03455a90404a208c2ef0fac59af5bf3311134
Martin Ankerl <martin.ankerl@gmail.com>
2021-06-15 08:30:45 +0200

    Updated sphinx to 4.0.2 and regenerated documentation
    mathjax inclusion is now smarter, only added where needed

--- src/docs/generate.sh
+++ src/docs/generate.sh
@@ -4,9 +4,13 @@
 #
 # Install breathe (assuming there's a venv in ~/git/venv)
 #   source ~/git/venv/bin/activate
-#   pip install breathe
-#   pip install sphinx_rtd_theme
-#   pip install recommonmark
+#   pip install -U sphinx
+#   pip install -U breathe
+#   pip install -U sphinx_rtd_theme
+#   pip install -U recommonmark
+
+# rm should work, this should copy all files, but for safety reasons I'm not doing it...
+#rm -Rvf ../../docs
 doxygen
 source ~/git/venv/bin/activate
 python ~/git/venv/bin/sphinx-build -E . ../../docs
